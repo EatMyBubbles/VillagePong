a2549433925b7b5b6d9172538b9e8efcf77b2ec6
Steven Zhao <szhao2399@gmail.com>
2015-08-05 17:06:00 -0400

    mostly done with sprite builder. still working on a mvp.

--- Source/MainScene.swift
+++ Source/MainScene.swift
@@ -1,5 +1,56 @@
 import Foundation
 
 class MainScene: CCNode {
-
+    
+    weak var ball: CCSprite!
+    weak var paddle: CCSprite!
+    weak var gamePhysicsNode: CCPhysicsNode!
+    
+    var width = CCDirector.sharedDirector().viewSize().width
+    var height = CCDirector.sharedDirector().viewSize().height
+    
+    func didLoadFromCCB() {
+        //multipleTouchEnabled = true
+        userInteractionEnabled = true
+        gamePhysicsNode.collisionDelegate = self
+    }
+    
+    func ballPush() {
+        var pushX = CGFloat(arc4random_uniform(201) - 100)
+        var pushY = CGFloat(200)
+        
+        ball.physicsBody.velocity.x = pushX
+        ball.physicsBody.velocity.y = pushY
+    }
+    
+    func spawnZombie() {
+        var spawnY = CGFloat(0.8 * height)
+        var xCoor = CGFloat(arc4random_uniform(3))
+        var zombie = CCBReader.load("Zombie") as! Zombie
+        
+        if xCoor == 0 {
+            zombie.position.x = 0.2 * width
+        }
+        else if xCoor == 1 {
+            zombie.position.x = 0.5 * width
+        }
+        else {
+            zombie.position.x = 0.8 * width
+        }
+        zombie.position.y = spawnY
+        
+        gamePhysicsNode.addChild(zombie)
+    }
+    
+    override func update(delta: CCTime) {
+        
+    }
+    
+    override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        
+    }
+    
+    override func touchMoved(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        
+    }
 }
---
+++ Zombie.swift
@@ -0,0 +1,13 @@
+//
+//  Zombie.swift
+//  VillageDelight
+//
+//  Created by Christopher Zhao on 8/5/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
+
+class Zombie: CCSprite {
+    
+}
