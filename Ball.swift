9e2ec6b9e22044c516351bcb688f786fba06dd2e
Steven Zhao <szhao2399@gmail.com>
2015-08-06 15:16:42 -0400

    first commit. don't do anything stupid. --me from the past

---
+++ Ball.swift
@@ -0,0 +1,13 @@
+//
+//  Ball.swift
+//  VillageDelight
+//
+//  Created by Christopher Zhao on 8/5/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
+
+class Ball: CCSprite {
+    
+}
---
+++ Source/MainScene.swift
@@ -0,0 +1,78 @@
+import Foundation
+
+class MainScene: CCNode {
+    
+    weak var ball: CCSprite!
+    weak var paddle: CCSprite!
+    weak var gamePhysicsNode: CCPhysicsNode!
+    
+    var width = CCDirector.sharedDirector().viewSize().width
+    var height = CCDirector.sharedDirector().viewSize().height
+    var mainMenu = true
+    var ballInScreen = false
+    
+    func didLoadFromCCB() {
+        //multipleTouchEnabled = true
+        userInteractionEnabled = true
+        gamePhysicsNode.collisionDelegate = self
+        
+        animationManager.runAnimationsForSequenceNamed("MainMenu")
+    }
+    
+    func play() {
+        animationManager.runAnimationsForSequenceNamed("GameStart")
+        ballPush()
+        mainMenu = false
+    }
+    
+    func ballPush() {
+        var pushX = CGFloat(arc4random_uniform(201)) - 100
+        var pushY = CGFloat(200)
+        
+        ball.physicsBody.velocity.x = pushX
+        ball.physicsBody.velocity.y = pushY
+    }
+    
+    func ballTrue() {
+        ballInScreen = true
+    }
+    
+    func spawnZombie() {
+        var spawnY = CGFloat(0.8 * height)
+        var xCoor = CGFloat(arc4random_uniform(3))
+        var zombie = CCBReader.load("Zombie") as! Zombie
+        
+        if xCoor == 0 {
+            zombie.position.x = 0.2 * width
+        }
+        else if xCoor == 1 {
+            zombie.position.x = 0.5 * width
+        }
+        else {
+            zombie.position.x = 0.8 * width
+        }
+        zombie.position.y = spawnY
+        
+        gamePhysicsNode.addChild(zombie)
+    }
+    
+    override func update(delta: CCTime) {
+        if ball.position.y < 0 && ballInScreen == true {
+            println("GameOver")
+        }
+    }
+    
+    override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        if mainMenu == false {
+            var touchX = touch.locationInNode(CCPhysicsNode()!).x
+            paddle.positionInPoints.x = touchX
+        }
+    }
+    
+    override func touchMoved(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        if mainMenu == false {
+            var touchX = touch.locationInNode(CCPhysicsNode()!).x
+            paddle.positionInPoints.x = touchX
+        }
+    }
+}
