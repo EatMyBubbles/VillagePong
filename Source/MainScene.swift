ea21913647c00f7764f02564aca15509c0b06214
Steven Zhao <szhao2399@gmail.com>
2015-09-14 21:35:58 -0400

    almost DOOOONE!!

--- Source/MainScene.swift
+++ Source/MainScene.swift
@@ -12,12 +12,13 @@ class MainScene: CCNode {
     weak var comboLabel: CCLabelTTF!
     weak var gameOverScoreLabel: CCLabelTTF!
     weak var highscoreLabel: CCLabelTTF!
+    weak var soundEffectsButtonText: CCLabelTTF!
+    weak var musicButtonText: CCLabelTTF!
     
     var width = CCDirector.sharedDirector().viewSize().width
     var height = CCDirector.sharedDirector().viewSize().height
     var mainMenu = true
     var ballInScreen = false
-    var zombieEnd = false
     var buttonPressed = true
     var randomSpawn = CGFloat(3)
     var hundredScore = 0
@@ -34,6 +35,10 @@ class MainScene: CCNode {
     }
     
     //OALSimpleAudio for sound
+    let audio = OALSimpleAudio.sharedInstance()
+    let defaults = NSUserDefaults.standardUserDefaults()
+    let soundEffectsKey = "soundEffectsKey"
+    let backgroundMusicKey = "backgroundMusicKey"
     
     func didLoadFromCCB() {
 //        multipleTouchEnabled = true
@@ -41,18 +46,59 @@ class MainScene: CCNode {
         gamePhysicsNode.collisionDelegate = self
 //        gamePhysicsNode.debugDraw = true
         
-        animationManager.runAnimationsForSequenceNamed("MainMenu")
+        mainMenuAnimation()
         setUpGameCenter()
-        preloadSounds()
+//        preloadSounds()
+        
+        if !defaults.boolForKey(soundEffectsKey) {
+            soundEffectsButtonText.string = "OFF"
+        }
+        
+        if !defaults.boolForKey(backgroundMusicKey) {
+            musicButtonText.string = "OFF"
+        }
+        
+        if defaults.boolForKey(soundEffectsKey) {
+            preloadSounds()
+        }
+        
+    }
+    
+    func soundEffectsToggle() {
+        var currentState = defaults.boolForKey(soundEffectsKey)
+        
+        if currentState {
+            defaults.setBool(false, forKey: soundEffectsKey)
+            soundEffectsButtonText.string = "OFF"
+        }
+        else {
+            defaults.setBool(true, forKey: soundEffectsKey)
+            soundEffectsButtonText.string = "ON"
+        }
+    }
+    
+    func backgroundMusicToggle() {
+        var currentState = defaults.boolForKey(backgroundMusicKey)
+        
+        if currentState {
+            defaults.setBool(false, forKey: backgroundMusicKey)
+            musicButtonText.string = "OFF"
+            audio.stopEverything()
+        }
+        else {
+            defaults.setBool(true, forKey: backgroundMusicKey)
+            musicButtonText.string = "ON"
+        }
     }
     
     func preloadSounds() {
         //access audio object
-        var hitSound: OALSimpleAudio = OALSimpleAudio.sharedInstance()
+//        var hitSound: OALSimpleAudio = OALSimpleAudio.sharedInstance()
         
         //preload the audio
-        hitSound.preloadEffect("realisticPunchMarkDiAngelo.mp3")
-
+        audio.preloadEffect("CRUNCH_sound.mp3")
+//        audio.preloadEffect("realisticPunchMarkDiAngelo.mp3")
+        println("sounds loaded")
     }
     
     func setUpGameCenter() {
@@ -60,8 +106,12 @@ class MainScene: CCNode {
         gameCenterInteractor.authenticationCheck()
     }
     
-    func openGameCenter() { //button for leaderboards - NEEDS IMPLEMENTING
+    func openGameCenter() { 
         showLeaderboard()
+    }
+    
+    func mainMenuAnimation() {
+        animationManager.runAnimationsForSequenceNamed("MainMenu")
     }
     
     func play() {
@@ -72,6 +122,10 @@ class MainScene: CCNode {
         mainMenu = false
         ballPush()
         
+        if defaults.boolForKey(backgroundMusicKey) {
+            audio.playBg("BackgroundMusic2.wav", loop: true) //http://www.flashkit.com/loops/Rap/Urban/alley-130626224514.html
+        }
+        
         iAdHandler.sharedInstance.displayBannerAd()
     }
     
@@ -81,8 +135,26 @@ class MainScene: CCNode {
     
     func settingsBack() { //currently a bit buggy . might get rid of settings button in restart timeline
         animationManager.runAnimationsForSequenceNamed("SettingsBack")
-//        sleep(1)
+
 //        animationManager.runAnimationsForSequenceNamed("MainMenu")
+    }
+    
+    func aboutTheGame() {
+        animationManager.runAnimationsForSequenceNamed("AboutTheGame")
+    }
+    
+    func backToSettings() {
+        animationManager.runAnimationsForSequenceNamed("BackToSettings")
+    }
+    
+    func backToMainMenu() {
+        mainMenu = true
+        zombies.removeAllChildren()
+        hundredScore = 0
+        score = 0
+        randomSpawn = 3
+        
+        animationManager.runAnimationsForSequenceNamed("BackToMain")
     }
     
     func ballPush() {
@@ -136,8 +208,11 @@ class MainScene: CCNode {
         hundredScore++
         scheduleOnce("comboHits", delay: 0.01)
         
-        var hitSound: OALSimpleAudio = OALSimpleAudio.sharedInstance()
-        hitSound.playEffect("realisticPunchMarkDiAngelo.mp3")
+//        var hitSound: OALSimpleAudio = OALSimpleAudio.sharedInstance()
+        if defaults.boolForKey(soundEffectsKey) {
+//            audio.playEffect("realisticPunchMarkDiAngelo.mp3")
+            audio.playEffect("CRUNCH_sound.mp3") //http://www.flashkit.com/soundfx/Cartoon/Crunches/CRUNCH_I-Intermed-566/index.php
+        }
         
         return false
     }
